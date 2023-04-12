378fce4398ffb4f619770135380e3124c7381449
Chetna Soni <chetnasoni040@gmail.com>
2023-04-13 03:15:39 +0530

    first commit

---
+++ backend/models/chatModel.js
@@ -0,0 +1,37 @@
+const mongoose = requuire('mongoose')
+
+const chatModel = mongoose.Schema({
+
+    chatName: { type: String, trim: true },
+    isGroupChat: { type: Boolean, default: false },
+    users: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+
+    },
+    ],
+    latestMessage:
+    {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Message",
+    },
+    groupAdmin: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+    },
+},
+    {
+        timestamps: true,
+    }
+);
+
+const Chat = mongoose.model("Chat", chatModel);
+mondule.exports = Chat;
+
+
+
+//chatName
+// isGroupCHat
+//users
+// latestMessage
+// groupAdmin
